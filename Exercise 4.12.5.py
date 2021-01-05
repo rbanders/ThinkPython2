c8ae2f633694f294d977d1353b7bf3e8b9c002d3
rbanders <rbanders@gmail.com>
2021-01-05 16:38:23 -0600

    Solutions to Exercise 4.12.5

---
+++ Exercise 4.12.5.py
@@ -0,0 +1,16 @@
+import turtle
+
+def draw_spiral(t, n, length = 3, a = 0.1, b=0.0002):
+    theta = 0.0
+
+    for i in range(n):
+        t.fd(length)
+        dtheta = 1/ (a+b*theta)
+
+        t.lt(dtheta)
+        theta += dtheta
+
+bob = turtle.Turtle()
+draw_spiral(bob, n=1000)
+
+turtle.mainloop()
