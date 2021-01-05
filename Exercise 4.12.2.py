f7220540e032b43d8edc57e10c1e308f4b47605a
rbanders <rbanders@gmail.com>
2021-01-05 11:00:19 -0600

    Solutions for Exercise 4.12.2

---
+++ Exercise 4.12.2.py
@@ -0,0 +1,60 @@
+import math
+import turtle
+
+
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
+
+def petal(t, r, angle):
+    for i in range(2):
+        arc(t, r, angle)
+        t.lt(180-angle)
+
+def flower(t, n, r, angle):
+    for i in range(n):
+        petal(t, r, angle)
+        t.lt(360.0/n)
+
+def move(t, length):
+    t.pu()
+    t.fd(length)
+    t.pd()
+
+bob = turtle.Turtle()
+
+move(bob, -100)
+flower(bob, 7, 60.0, 60)
+
+move(bob, 100)
+flower(bob, 10, 40.0, 80)
+
+move(bob, 100)
+flower(bob, 20, 140.0, 20)
+
+bob.hideturtle()
+turtle.mainloop()
