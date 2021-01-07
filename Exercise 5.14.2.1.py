15e0392e1da9638c46e226b1b66e4f65020df080
rbanders <rbanders@gmail.com>
2021-01-07 14:55:33 -0600

    Solution to Exercise 5.14.2

---
+++ Exercise 5.14.2.1.py
@@ -0,0 +1,9 @@
+import math
+
+def check_fermat(a, b, c, n):
+    if n > 2 and (a**n + b**n == c**n):
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn't work")
+
+check_fermat(1, 2, 3, 4)
