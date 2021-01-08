ebd8cf8aac037142bf1e7b40fa2dc07e6eae2b0a
rbanders <rbanders@gmail.com>
2021-01-08 13:11:43 -0600

    Solutions to Exercise 5.14.5

---
+++ Exercise 5.14.5.py
@@ -0,0 +1,39 @@
+"""Exercise 5
+Read the following function and see if you can figure out
+what it does (see the examples in Chapter 4).
+
+# infinite recursion?
+
+Then run it and see if you got it right.
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.fd(length*n)
+    t.lt(angle)
+    draw(t, length, n-1)
+    t.rt(2*angle)
+    draw(t, length, n-1)
+    t.lt(angle)
+    t.bk(length*n)"""
+import turtle
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.fd(length*n)
+    t.lt(angle)
+    draw(t, length, n-1)
+    t.rt(2*angle)
+    draw(t, length, n-1)
+    t.lt(angle)
+    t.bk(length*n)
+
+bob = turtle.Turtle()
+draw(bob, 10, 10)
+
+turtle.mainloop()
+
+# makes a lung/kidney shaped object
