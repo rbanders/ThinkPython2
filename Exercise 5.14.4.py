0d2cee183914b3051fa16fb087cf3366eea91909
rbanders <rbanders@gmail.com>
2021-01-08 12:57:19 -0600

    Solutions to Exercise 5.14.4

---
+++ Exercise 5.14.4.py
@@ -0,0 +1,49 @@
+"""Exercise 4   What is the output of the following program?
+Draw a stack diagram that shows the state of the program when it prints the result.
+
+def recurse(n, s):
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+recurse(3, 0)
+
+1. What would happen if you called this function like this: recurse(-1, 0)?
+# infinite recursion
+2. Write a docstring that explains everything someone would need to know
+in order to use this function (and nothing else)."""
+
+"""
+
+__Main__:
+    None
+
+recurse:
+    n--->3
+    s--->0
+
+recurse:
+    n--->2
+    s--->3
+
+recurse:
+    n--->1
+    s--->5
+
+recurse:
+    n--->0
+    s--->6
+"""
+
+def recurse(n, s):
+    """Recursive function for getting and printing the value of s;
+    n = positive integer;
+    s = integer
+    """
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+recurse(3, 0)
